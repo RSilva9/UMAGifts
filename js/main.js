74cc20174381a87f09dfe5f13dbe587980e3e97f
RSilva9 <ramasilva909@gmail.com>
2022-12-27 18:18:58 -0300

    Filtros terminados y responsive

--- js/main.js
+++ js/main.js
@@ -1,87 +1,209 @@
-let container = document.getElementById("cardCont")
+// const cards = document.querySelectorAll(".cardd")
 
+// for(let i=0; i<3; i+=1){
+//     if(window.innerWidth < 768){
+//         cards[0].setAttribute('id','cardCont')
+//     }else if(window.innerWidth >= 768){
+//         cards[1].setAttribute('id','cardCont')
+//     }
+// }
 
-if(window.location.pathname=='/productos.html'){
+// window.onresize = ()=>{
+//     for(let i=0; i<3; i+=1){
+//         if(window.innerWidth < 768){
+//             cards[0].setAttribute('id','cardCont')
+//             cards[1].removeAttribute('id','cardCont')
+//         }else if(window.innerWidth >= 768){
+//             cards[0].removeAttribute('id','cardCont')
+//             cards[1].setAttribute('id','cardCont')
+//         }
+//     }
+// }
+
+let container = document.querySelector("#cardCont")
+
+function renderAll() {
+    container.innerHTML = ""
     
     fetch("./json/box.json")
-    .then((res)=>res.json())
-    .then((boxs)=>{
-        for(let i=0; i<boxs.length; i+=1){
-            let cardRow = document.createElement('div')
-            
-            cardRow.innerHTML += `
-            <div class="cCard wow fadeInDown" data-wow-delay="0.2s">
+        .then((res) => res.json())
+        .then((boxs) => {
+            for (let i = 0; i < boxs.length; i += 1) {
+                let cardRow = document.createElement('div')
+
+                cardRow.innerHTML += `
+            <div class="cCard">
             <img src="${boxs[i].img}" alt="...">
             <a class="buttn btnProds" data-name=${boxs[i].codigo}>Ver más</a>
             </div>
-            `  
-            cardRow.className= "col-6 col-md-4 p-0"
-            container.append(cardRow)
+            `
+                cardRow.className = "col-6 col-md-4 p-0"
+                container.append(cardRow)
 
-        }
-       
-        
-    })
-    .finally(()=>{
-        const buttons = document.getElementsByClassName("btnProds")
-        SwalBtns(buttons)
-    })
+            }
+        })
+        .finally(() => {
+            const buttons = document.getElementsByClassName("btnProds")
+            SwalBtns(buttons)
+        })
 }
 
-const SwalBtns = (buttons)=>{
-    for(let btn of buttons){
-        btn.onclick = ()=>{
+if (window.location.pathname == '/productos.html') {
+    renderAll()
+}
+
+const SwalBtns = (buttons) => {
+    for (let btn of buttons) {
+        btn.onclick = () => {
             fetch("./json/box.json")
-            .then((res)=>res.json())
-            .then((boxs)=>{
-                for(let box of boxs){
-                    if(btn.dataset.name == box.codigo){
-                        Swal.fire({
-                            html: box.text,
-                            showCloseButton: true,
-                            confirmButtonText:'<i class="fa fa-thumbs-up"></i> Ver en MercadoShops',
-                        }).then((res)=>{
-                            if(res.isConfirmed){
-                                window.open(box.link)
-                            }
-                        })
+                .then((res) => res.json())
+                .then((boxs) => {
+                    for (let box of boxs) {
+                        if (btn.dataset.name == box.codigo) {
+                            Swal.fire({
+                                html: box.text,
+                                showCloseButton: true,
+                                confirmButtonText: '<i class="fa fa-thumbs-up"></i> Ver en MercadoShops',
+                            }).then((res) => {
+                                if (res.isConfirmed) {
+                                    window.open(box.link)
+                                }
+                            })
+                        }
                     }
-                }
-            })    
+                })
         }
     }
 }
 
-btnPrueba = document.getElementById("test")
-btnPrueba.onclick = ()=>{
-    
-    fetch("./json/box.json")
-    .then((res)=>res.json())
-    .then((boxs)=>{
-        container.innerHTML = ""
-        for(let box of boxs){
-            if(box.tam == "doble"){
-                
-                let cardRow = document.createElement('div')
+// FILTROS
 
-                cardRow.innerHTML += `
-                <div class="cCard wow fadeInDown" data-wow-delay="0.2s">
-                <img src="${box.img}" alt="...">
-                <a class="buttn btnProds" data-name=${box.codigo}>Ver más</a>
-                </div>
-                `  
-                cardRow.className= "col-6 col-md-4 p-0"
-                container.append(cardRow)
-                
-            }else{
-                console.log("error")
-            }
+const tipo = document.getElementsByName("tipo")
+const tam = document.getElementsByName("tam")
+let deli = document.getElementById("deli")
+let btnReset = document.getElementById("bReset")
+
+btnReset.onclick = () => {
+    for (let t of tipo) {
+        t.disabled = false
+        t.checked = false
+    }
+    for (let tm of tam) {
+        tm.disabled = false
+        tm.checked = false
+    }
+    deli.checked = false
+    renderAll()
+}
+
+deli.addEventListener('change', () => {
+    if (deli.checked) {
+        for (let t of tipo) {
+            t.checked = false
+            t.disabled = true
         }
-       
-        
+        for (let tm of tam) {
+            tm.checked = false
+            tm.disabled = true
+        }
+        container.innerHTML = ""
+        fetch("./json/box.json")
+            .then((res) => res.json())
+            .then((boxs) => {
+                for (let box of boxs) {
+                    if (box.deli == true) {
+                        let cardRow = document.createElement('div')
+
+                        cardRow.innerHTML += `
+                    <div class="cCard wow fadeInDown" data-wow-delay="0.1s" data-name=${box.tipo}>
+                    <img src="${box.img}" alt="...">
+                    <a class="buttn btnProds" data-name=${box.codigo}>Ver más</a>
+                    </div>
+                    `
+                        cardRow.className = "col-6 col-md-4 p-0"
+                        container.append(cardRow)
+                    }
+                }
+
+            })
+            .finally(() => {
+                const buttons = document.getElementsByClassName("btnProds")
+                SwalBtns(buttons)
+            })
+
+    } else {
+        for (let t of tipo) {
+            t.disabled = false
+        }
+        for (let tm of tam) {
+            tm.disabled = false
+        }
+        renderAll()
+    }
+})
+
+for (let t of tipo) {
+    t.addEventListener('change', () => {
+        if (t.checked)
+            tipYtam(t)
     })
-    .finally(()=>{
-        const buttons = document.getElementsByClassName("btnProds")
-        SwalBtns(buttons)
-    })
+
+}
+
+function tipYtam(t, del) {
+    for (let tm of tam) {
+        if (tm.checked) {
+            renderTipo(t.id, tm.id)
+            return;
+        } else if (!(tm.checked)) {
+            renderTipo(t.id, "asd")
+        }
+
+        tm.addEventListener('change', () => {
+            if (tm.checked) {
+                renderTipo(t.id, tm.id)
+            }
+        })
+    }
+}
+
+function renderTipo(tId, tmId, del) {
+    fetch("./json/box.json")
+        .then((res) => res.json())
+        .then((boxs) => {
+            container.innerHTML = ""
+            for (let box of boxs) {
+                if (tmId == "asd") {
+                    if (box.tipo == tId) {
+                        let cardRow = document.createElement('div')
+
+                        cardRow.innerHTML += `
+                    <div class="cCard wow fadeInDown" data-wow-delay="0.1s" data-name=${box.tipo}>
+                    <img src="${box.img}" alt="...">
+                    <a class="buttn btnProds" data-name=${box.codigo}>Ver más</a>
+                    </div>
+                    `
+                        cardRow.className = "col-6 col-md-4 p-0"
+                        container.append(cardRow)
+                    }
+                } else if (tmId != "asd") {
+                    if (box.tipo == tId && box.tam == tmId) {
+                        let cardRow = document.createElement('div')
+
+                        cardRow.innerHTML += `
+                        <div class="cCard wow fadeInDown" data-wow-delay="0.1s" data-name=${box.tipo}>
+                        <img src="${box.img}" alt="...">
+                        <a class="buttn btnProds" data-name=${box.codigo}>Ver más</a>
+                        </div>
+                        `
+                        cardRow.className = "col-6 col-md-4 p-0"
+                        container.append(cardRow)
+                    }
+                }
+            }
+        })
+        .finally(() => {
+            const buttons = document.getElementsByClassName("btnProds")
+            SwalBtns(buttons)
+        })
 }
