f0f1294e0e3754267fb7b8bceb1d4bb2afef3162
RSilva9 <ramasilva909@gmail.com>
2023-01-03 20:41:38 -0300

    Armá tu box FUNCIONAL !!

--- js/customBox.js
+++ js/customBox.js
@@ -6,6 +6,9 @@ let accCajas = document.querySelector("#accCajas")
 let accVinos = document.querySelector("#accVinos")
 let accDelis = document.querySelector("#accDelis")
 
+let btnEnd = document.getElementById("btnEnd")
+var checkeadas = []
+
 const cajaFinal = {
     estuche: "",
     bebida: {
@@ -15,9 +18,12 @@ const cajaFinal = {
     deli: {
         deliUno: "",
         deliDos: "",
-        deliTres: ""
+        deliTres: "",
+        deliCuatro: ""
     }
 }
+
+const deliArr = Object.values(cajaFinal.deli)
 
 renderProds()
 
@@ -58,7 +64,7 @@ function renderProds() {
                     let cards = document.createElement("div")
                     cards.innerHTML =
                     `
-                    <div class="cCard h-100 pb-3 px-2" id="deli">
+                    <div class="cCard h-100 pb-3 px-2" id="deli" data-size=${item.size}>
                     <input id="cajaInput" type="checkbox" name="deli" class="stretched-link" data-code=${item.codigo}>
                     <img src="${item.img}" alt="...">
                     <h2>${item.nombre}</h2>
@@ -86,12 +92,11 @@ const checker = (arr)=>{
 }
 
 const listener = (cajas, vinos, delics)=>{
-    
-    var checkeadas = []
+     
     const limit = 2
-    const limitD = 6
-    let suma = 0
-
+    const limitD = 34
+    var suma = 0
+    
     for(let c of cajas){       
 
         c.addEventListener("change", ()=>{
@@ -107,12 +112,28 @@ const listener = (cajas, vinos, delics)=>{
                         checker(vinos)
                         checker(cajas)
                     }
+                    for(let d of delics){
+                        if(d.checked){
+                            d.checked = false
+                            d.parentElement.classList.remove("cardColorCheck")
+                        }
+                    }
+                    acItemTres.parentElement.classList.add("d-none")
+                    acItemTres.classList.remove("show")
+
                 }else
                 if(c.parentElement.dataset.tam == "doble"){
                     for(let v of vinos){
                         v.type = "checkbox"
+
+                        checkeadas = []
+                        
+                        checker(vinos)
+                        checker(cajas)
                     }
                 }
+                cajaFinal.bebida.vinoUno = ""
+                cajaFinal.bebida.vinoDos = ""
                 acItemDos.parentElement.classList.remove("d-none")
                 acItemDos.classList.add("show")
             }
@@ -121,6 +142,8 @@ const listener = (cajas, vinos, delics)=>{
                 v.checked = false
                 v.parentElement.classList.remove("cardColorCheck")
             }
+            btnEnd.parentElement.classList.add("d-none")
+            btnEnd.parentElement.classList.remove("d-flex")
         })
     }
 
@@ -130,9 +153,10 @@ const listener = (cajas, vinos, delics)=>{
             if(v.checked){
                 if(checkeadas.length < limit){
                     v.parentElement.classList.add("cardColorCheck")
-                    acItemTres.parentElement.classList.remove("d-none")
-                    acItemTres.classList.add("show")
+                    
                     if(v.type == "checkbox"){
+                        acItemTres.parentElement.classList.remove("d-none")
+                        acItemTres.classList.add("show")
                         checkeadas.push(v)
                         if(cajaFinal.bebida.vinoUno == ""){
                             cajaFinal.bebida.vinoUno = v.parentElement.childNodes[5].textContent
@@ -142,9 +166,11 @@ const listener = (cajas, vinos, delics)=>{
                         
                     }else
                     if(v.type == "radio"){
-                        cajaFinal.bebida = v.parentElement.childNodes[5].textContent
+                        cajaFinal.bebida.vinoUno = v.parentElement.childNodes[5].textContent
                         acItemTres.parentElement.classList.add("d-none")
                         acItemTres.classList.remove("show")
+                        btnEnd.parentElement.classList.remove("d-none")
+                        btnEnd.parentElement.classList.add("d-flex")
                     }
                 }else{
                     v.checked = false
@@ -152,10 +178,12 @@ const listener = (cajas, vinos, delics)=>{
                 if(checkeadas.length == limit){
                     acItemTres.parentElement.classList.add("d-none")
                     acItemTres.classList.remove("show")
+                    btnEnd.parentElement.classList.remove("d-none")
+                    btnEnd.parentElement.classList.add("d-flex")
                 }
             }else{
                 checkeadas.pop(v)
-                if(checkeadas.length < limit){
+                if(checkeadas.length == 1){
                     acItemTres.parentElement.classList.remove("d-none")
                     acItemTres.classList.add("show")
                 }
@@ -171,37 +199,250 @@ const listener = (cajas, vinos, delics)=>{
                     cajaFinal.bebida.vinoUno = cajaFinal.bebida.vinoDos
                     cajaFinal.bebida.vinoDos = ""
                 }
+                btnEnd.parentElement.classList.add("d-none")
+                btnEnd.parentElement.classList.remove("d-flex")
             }
             checker(vinos)
-            console.log(checkeadas.length)
+            cajaFinal.deli.deliUno = ""
+            cajaFinal.deli.deliDos = ""
+            cajaFinal.deli.deliTres = ""
+            cajaFinal.deli.deliCuatro = ""
+            for(let i=0; i<deliArr.length; i+=1){
+                deliArr[i] = ""
+            }
+            for(let d of delics){
+                d.parentElement.classList.remove("cardColorCheck")
+                d.checked = false
+            }
+            suma = 0
         })
     }
 
     for(let d of delics){
-        
 
         d.addEventListener("change", ()=>{
+            if(d.checked){         
+                if((suma + Number(d.parentElement.dataset.size)) > limitD){
+                    d.checked = false
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'No hay espacio en la BOX para este producto',
+                        text: 'Seleccione o reemplacelo por otro',
+                      })
+                }else{
+                    d.parentElement.classList.add("cardColorCheck")
+                    suma += Number(d.parentElement.dataset.size)
+
+                    if(deliArr[0] == ""){
+                        deliArr[0] = d.parentElement.childNodes[5].textContent
+                        cajaFinal.deli.deliUno = d.parentElement.childNodes[5].textContent
+                    }else
+                    if(deliArr[1] == ""){
+                        deliArr[1] = d.parentElement.childNodes[5].textContent
+                        cajaFinal.deli.deliDos = d.parentElement.childNodes[5].textContent
+                    }else
+                    if(deliArr[2] == ""){
+                        deliArr[2] = d.parentElement.childNodes[5].textContent
+                        cajaFinal.deli.deliTres = d.parentElement.childNodes[5].textContent
+                    }else
+                    if(deliArr[3] == ""){
+                        deliArr[3] = d.parentElement.childNodes[5].textContent
+                        cajaFinal.deli.deliCuatro = d.parentElement.childNodes[5].textContent
+                    }
+                }
+            }else{
+                if(deliArr[3] == d.parentElement.childNodes[5].textContent){
+                    deliArr[3] = ""
+                    cajaFinal.deli.deliCuatro = ""
+                }else
+                if(deliArr[2] == d.parentElement.childNodes[5].textContent){
+                    deliArr[2] = deliArr[3]
+                    deliArr[3] = ""
+                    cajaFinal.deli.deliTres = cajaFinal.deli.deliCuatro
+                    cajaFinal.deli.deliCuatro = ""
+                }else
+                if(deliArr[1] == d.parentElement.childNodes[5].textContent){
+                    deliArr[1] = deliArr[2]
+                    deliArr[2] = deliArr[3]
+                    deliArr[3] = ""
+                    cajaFinal.deli.deliDos = cajaFinal.deli.deliTres
+                    cajaFinal.deli.deliTres = cajaFinal.deli.deliCuatro
+                    cajaFinal.deli.deliCuatro = ""
+                }else
+                if(deliArr[0] == d.parentElement.childNodes[5].textContent){
+                    deliArr[0] = deliArr[1]
+                    deliArr[1] = deliArr[2]
+                    deliArr[2] = deliArr[3]
+                    deliArr[3] = ""
+                    cajaFinal.deli.deliUno = cajaFinal.deli.deliDos
+                    cajaFinal.deli.deliDos = cajaFinal.deli.deliTres
+                    cajaFinal.deli.deliTres = cajaFinal.deli.deliCuatro
+                    cajaFinal.deli.deliCuatro = ""
+                }
             
+                d.parentElement.classList.remove("cardColorCheck")
+                suma -= Number(d.parentElement.dataset.size)
+            }
+            if(suma == 0){
+                btnEnd.parentElement.classList.add("d-none")
+                btnEnd.parentElement.classList.remove("d-flex")
+            }else{
+                btnEnd.parentElement.classList.remove("d-none")
+                btnEnd.parentElement.classList.add("d-flex")
+            }
         })
     }
 }
 
+btnEnd.addEventListener("click", ()=>{
+    let cfContainer = document.createElement("div")
+    cfContainer.classList.add("d-flex")
+    cfContainer.classList.add("flex-column")
+    cfContainer.classList.add("cfSwal")
 
+    if(checkeadas.length == 1){
 
-
-
-function RenderCajaFinal(checkeadas){
-    let cfContainer = document.getElementById("cajaFinalContainer")
+        if(cajaFinal.deli.deliCuatro != ""){
+            cfContainer.innerHTML = 
+            `
+            <h2>CAJA FINAL</h2>
+        
+            <div class="d-flex flex-row">
+            <h3 class="me-1 fw-bold">Estuche:</h3>
+            <h3> ${cajaFinal.estuche}<h3>
+            </div>
+        
+            <div class="d-flex flex-row">
+            <h3 class="me-1 fw-bold">Botella:</h3>
+            <h3>${cajaFinal.bebida.vinoUno}</h3>
+            </div>
+    
+            <div class="d-flex flex-row">
+            <h3 class="me-1 fw-bold">Delicatessen:</h3>
+                <div>
+                <h3>○ ${cajaFinal.deli.deliUno}</h3>
+                <h3>○ ${cajaFinal.deli.deliDos}</h3>
+                <h3>○ ${cajaFinal.deli.deliTres}</h3>
+                <h3>○ ${cajaFinal.deli.deliCuatro}</h3>
+                </div>
+            </div>
+            `
+        }else
+        if(cajaFinal.deli.deliTres != ""){
+            cfContainer.innerHTML = 
+            `
+            <h2>CAJA FINAL</h2>
+        
+            <div class="d-flex flex-row">
+            <h3 class="me-1 fw-bold">Estuche:</h3>
+            <h3> ${cajaFinal.estuche}<h3>
+            </div>
+        
+            <div class="d-flex flex-row">
+            <h3 class="me-1 fw-bold">Botella:</h3>
+            <h3>${cajaFinal.bebida.vinoUno}</h3>
+            </div>
+    
+            <div class="d-flex flex-row">
+            <h3 class="me-1 fw-bold">Delicatessen:</h3>
+                <div>
+                <h3>○ ${cajaFinal.deli.deliUno}</h3>
+                <h3>○ ${cajaFinal.deli.deliDos}</h3>
+                <h3>○ ${cajaFinal.deli.deliTres}</h3>
+                </div>
+            </div>
+            `
+        }else
+        if(cajaFinal.deli.deliDos != ""){
+            cfContainer.innerHTML = 
+            `
+            <h2>CAJA FINAL</h2>
+        
+            <div class="d-flex flex-row">
+            <h3 class="me-1 fw-bold">Estuche:</h3>
+            <h3> ${cajaFinal.estuche}<h3>
+            </div>
+        
+            <div class="d-flex flex-row">
+            <h3 class="me-1 fw-bold">Botella:</h3>
+            <h3>${cajaFinal.bebida.vinoUno}</h3>
+            </div>
+    
+            <div class="d-flex flex-row">
+            <h3 class="me-1 fw-bold">Delicatessen:</h3>
+                <div>
+                <h3>○ ${cajaFinal.deli.deliUno}</h3>
+                <h3>○ ${cajaFinal.deli.deliDos}</h3>
+                </div>
+            </div>
+            `
+        }else
+        if(cajaFinal.deli.deliUno != ""){
+            cfContainer.innerHTML = 
+            `
+            <h2>CAJA FINAL</h2>
+        
+            <div class="d-flex flex-row">
+            <h3 class="me-1 fw-bold">Estuche:</h3>
+            <h3> ${cajaFinal.estuche}<h3>
+            </div>
+        
+            <div class="d-flex flex-row">
+            <h3 class="me-1 fw-bold">Botella:</h3>
+            <h3>${cajaFinal.bebida.vinoUno}</h3>
+            </div>
+    
+            <div class="d-flex flex-row">
+            <h3 class="me-1 fw-bold">Delicatessen:</h3>
+                <div>
+                <h3>○ ${cajaFinal.deli.deliUno}</h3>
+                </div>
+            </div>
+            `
+        }
+    
+    }else
     if(checkeadas.length == 2){
-        cfContainer.innerHTML=`
-        <h3>Estuche: ${cajaFinal.estuche}</h3>
-        <h3>Bebidas: ${cajaFinal.bebida.vinoUno}, <br>${cajaFinal.bebida.vinoDos}</h3>
+        cfContainer.innerHTML = 
+        `
+        <h2>CAJA FINAL</h2>
+    
+        <div class="d-flex flex-row align-items-center">
+        <h3 class="me-1 fw-bold">Estuche:</h3>
+        <h3>${cajaFinal.estuche}<h3>
+        </div>
+    
+        <div class="d-flex flex-row">
+        <h3 class="me-1 fw-bold">Botellas:</h3>
+            <div>
+            <h3>${cajaFinal.bebida.vinoUno}</h3>
+            <h3>${cajaFinal.bebida.vinoDos}</h3>
+            </div>
+        </div>
         `
     }else{
-        cfContainer.innerHTML=`
-        <h3>Estuche: ${cajaFinal.estuche}</h3>
-        <h3>Bebida: ${cajaFinal.bebida}</h3>
+        cfContainer.innerHTML = 
+        `
+        <h2>CAJA FINAL</h2>
+    
+        <div class="d-flex flex-row align-items-center">
+        <h3 class="me-1 fw-bold">Estuche:</h3>
+        <h3>${cajaFinal.estuche}<h3>
+        </div>
+    
+        <div class="d-flex flex-row">
+        <h3 class="me-1 fw-bold">Botella:</h3>
+            <div>
+            <h3>${cajaFinal.bebida.vinoUno}</h3>
+            </div>
+        </div>
         `
     }
     
-}
+
+    Swal.fire({
+        html: cfContainer,
+        showCloseButton: true,
+        confirmButtonText: '<i class="fa fa-thumbs-up"></i> Listo',
+    })
+})
